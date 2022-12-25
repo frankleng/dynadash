9e00a781cf7805af444c92fd133053cdf2662f8a
Frank Leng <email@frankleng.me>
2022-12-24 22:06:50 -0800

    v1.8.1 export updateTableRow

--- src/index.ts
+++ src/index.ts
@@ -4,22 +4,23 @@ export * from "./constants";
 
 export { marshall, unmarshall } from "@aws-sdk/util-dynamodb";
 
+// types
 export type { QueryOutput, QueryInput, BatchWriteItemOutput, BatchGetItemOutput } from "@aws-sdk/client-dynamodb";
-
 export type { KeyCondMap, KeyCondExpressionMap, FilterExpressionMap } from "./types";
 
+// client, utils, constants
 export { setClient, ddbClientInstance, getDdbClient } from "./client";
-
-export { queryTableIndex, queryTable } from "./query";
-
-export { putTableRow, delTableRow } from "./write";
-
-export { batchDelTable, batchPutTable } from "./batchWrite";
-
-export { getTableRow } from "./get";
-
 export { consoleError, consoleLog, chunkList, batchWriteTable } from "./utils";
-
 export { DEFAULT_MARSHALL_OPTIONS } from "./constants";
 
+// write, batch write
+export { putTableRow, delTableRow } from "./write";
+export { batchDelTable, batchPutTable } from "./batchWrite";
+
+// get, query
+export { getTableRow } from "./get";
+export { queryTableIndex, queryTable } from "./query";
+
+// shallow update, update
 export { shallowUpdateTableRow } from "./shallowUpdate";
+export { updateTableRow } from "./update";
