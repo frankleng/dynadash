ed3f35dff3e7ae8557580a6a18bd177e24d8d87d
Frank Leng <email@frankleng.me>
2023-08-27 22:59:10 -0700

    remove http fix for now. seems unnecessary now that most fixes have been merged into main. also having http/smithy breaks compat w/ Cf worker

--- src/client.ts
+++ src/client.ts
@@ -1,7 +1,4 @@
 import { DynamoDBClient, DynamoDBClientConfig } from "@aws-sdk/client-dynamodb";
-import { NodeHttpHandler } from "@aws-sdk/node-http-handler";
-import https from "https";
-import http from "http";
 
 export let ddbClientInstance: DynamoDBClient | null = null;
 
@@ -13,16 +10,6 @@ export function initDdbClient(
     ddbClientInstance = new DynamoDBClient({
       credentialDefaultProvider,
       maxAttempts,
-      requestHandler: new NodeHttpHandler({
-        socketTimeout: 10000,
-        connectionTimeout: 10000,
-        httpsAgent: new https.Agent({
-          maxSockets: 50,
-        }),
-        httpAgent: new http.Agent({
-          maxSockets: 50,
-        }),
-      }),
     });
   }
   return ddbClientInstance;
